3f94642afe8241ff1a921642ce7b2454d15d21a7
sujithp-anker <sujith.parthiban@ankercloud.com>
2026-03-10 21:14:30 +0530

    tf files

--- aws/templates/onboarding/main.tf
+++
@@ -1,8 +0,0 @@
-module "phase_1_sns" {
-  source = "../../modules/sns-alerts"
-  
-  count  = var.enable_phase_1_alerts ? 1 : 0
-
-  customer_name = var.customer_name
-  alert_email   = var.alert_email
-}
--- aws/templates/onboarding/providers.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-
-#   assume_role {
-#     role_arn     = "arn:aws:iam::${var.customer_account_id}:role/OrganizationAccountAccessRole"
-#     session_name = "GaiaOnboardingSession"
-#   }
-}
