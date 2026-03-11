768dd93bc173c03bf024d27cc229bfc76b5fb5d3
sujithp-anker <sujith.parthiban@ankercloud.com>
2026-03-11 17:43:19 +0530

    tf files

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 module "sns" {
   source = "./modules/sns-alerts"
-  count  = var._01_enable_alerts ? 1 : 0
+  count  = var.EnableAlerts ? 1 : 0
 
-  customer_name = var._00_customer_name
-  alert_email   = var._01_alert_email
+  customer_name = var.CustomerName
+  alert_email   = var.AlertEmail
 }
 
 module "iam_security" {
   source = "./modules/iam-security"
   
-  count  = (var._02_enable_mfa_enforcement || var._02_enable_password_policy) ? 1 : 0
+  count  = (var.EnableMFAEnforcement || var.EnablePasswordPolicy) ? 1 : 0
 
-  customer_name          = var._00_customer_name
-  enable_mfa             = var._02_enable_mfa_enforcement
-  enable_password_policy = var._02_enable_password_policy
+  customer_name          = var.CustomerName
+  enable_mfa             = var.EnableMFAEnforcement
+  enable_password_policy = var.EnablePasswordPolicy
 }
