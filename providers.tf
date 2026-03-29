1eab76a96f9d548796d1969833052184e940eba5
sujithp-anker <sujith.parthiban@ankercloud.com>
2026-03-29 23:21:13 +0530

    modify changes in vpc

--- providers.tf
+++ providers.tf
@@ -1,11 +1,11 @@
-provider "aws" {
-  region = var.Region
+# provider "aws" {
+#   region = var.Region
 
-  # dynamic "assume_role" {
-  #   for_each = var.CrossAccountAssumeRoleARN != "" ? [1] : []
-  #   content {
-  #     role_arn     = var.CrossAccountAssumeRoleARN
-  #     session_name = "OnboardingSession"
-  #   }
-  # }
-}
+#   # dynamic "assume_role" {
+#   #   for_each = var.CrossAccountAssumeRoleARN != "" ? [1] : []
+#   #   content {
+#   #     role_arn     = var.CrossAccountAssumeRoleARN
+#   #     session_name = "OnboardingSession"
+#   #   }
+#   # }
+# }
