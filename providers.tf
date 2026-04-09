aa625486e005238e777f4c17128f56f949ec7679
sujithp-anker <sujith.parthiban@ankercloud.com>
2026-04-09 15:52:03 +0530

    some modifications

--- providers.tf
+++ providers.tf
@@ -1,7 +1,3 @@
 provider "aws" {
-  region = "eu-central-1"
-  assume_role {
-    role_arn     = "arn:aws:iam::546010718980:role/AdminAccessRole"
-    session_name = "gaia-session"
-  }
+  region = var.Region
 }
