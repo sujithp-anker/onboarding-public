2b22d49655513a4aa3a726cf3bd8317afcfd98de
sujithp-anker <sujith.parthiban@ankercloud.com>
2026-04-09 16:01:30 +0530

    some modifications

--- modules/publicport-alerts/main.tf
+++ modules/publicport-alerts/main.tf
@@ -2,6 +2,7 @@ resource "aws_config_config_rule" "public_port_check" {
   count       = var.enable_public_ports_alerts ? 1 : 0
   name        = "${var.customer_name}-public-port-check"
   description = "Checks for Security Groups with unrestricted (0.0.0.0/0) access to non-web ports."
+  region = var.region
 
   source {
     owner             = "AWS"
@@ -17,6 +18,8 @@ resource "aws_cloudwatch_event_rule" "public_port_violation" {
   count       = var.enable_public_ports_alerts ? 1 : 0
   name        = "${var.customer_name}-public-port-violation"
   description = "Triggers when a Security Group is found with unauthorized public ports"
+  region = var.region
+  
 
   event_pattern = jsonencode({
     source      = ["aws.config"]
@@ -31,6 +34,7 @@ resource "aws_cloudwatch_event_rule" "public_port_violation" {
 }
 
 resource "aws_cloudwatch_event_target" "sns_public_port" {
+  region = var.region
   count     = var.enable_public_ports_alerts ? 1 : 0
   rule      = aws_cloudwatch_event_rule.public_port_violation[0].name
   target_id = "SendToSNS"
