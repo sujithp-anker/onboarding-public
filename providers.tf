622441d174407905d47b1e360844a45d314853bf
sujithp-anker <sujith.parthiban@ankercloud.com>
2026-04-09 15:44:57 +0530

    some modifications

--- providers.tf
+++ providers.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  alias  = "frankfurt" 
+  region = "eu-central-1"
+}
+
 provider "aws" {
   region = "eu-central-1"
 }
