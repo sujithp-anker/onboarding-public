de3a6c2d7631ca5ac706954e5caa4c32253ad22f
sujithp-anker <sujith.parthiban@ankercloud.com>
2026-04-09 15:19:34 +0530

    some modifications

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "CustomerAccountId" {
 
 variable "Region" { 
   type        = string
-  default     = "us-east-1"
+  default     = ""
   description = "The AWS Region where the servers and databases are located."
 }
 
